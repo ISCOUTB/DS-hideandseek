1845b173b2214d0e1eb2c2d87ed78e045f36de14
Juan Eduardo <juaneduardocai28@gmail.com>
2025-03-25 20:47:37 -0500

    Actualizar hide_n_seek.dart y main.dart con los cambios recientes

---
+++ lib/game/hide_n_seek.dart
@@ -0,0 +1,17 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+class HideNseek extends FlameGame {
+  @override
+  Future<void> onLoad() async {}
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+  }
+}
